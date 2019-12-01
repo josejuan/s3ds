c5b5a85d4529be9b4a727a7d63f6e9c8dc15a686
josejuan <dev@computer-mind.com>
2019-12-01 18:44:49 +0100

    up!

--- recon/src/main/java/com/s3ds/recon/Main.java
+++ recon/src/main/java/com/s3ds/recon/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(final String... args) throws IOException {
 
         final Frustum camera = new Frustum('1', new float[]{0.8f, 0.0f, -2.0f}, new float[]{0, 0, 0.5f}, new float[]{0, 1, 0}, 3.57f, 1.97f);
-        final Frustum projector = new Frustum('2', new float[]{0, 0, -2.0f}, new float[]{0, 0, 0.5f}, new float[]{0, 1, 0}, 1.6f, 0.9f);
+        final Frustum projector = new Frustum('2', new float[]{-0.2f, 0, -1.0f}, new float[]{0, 0, 0.5f}, new float[]{0, 1, 0}, 1.6f, 0.9f);
 
         final ScanLine sequence = new ScanLine(args[0]);
         final ScanLineRecon recon = new ScanLineRecon(sequence, projector, camera);
